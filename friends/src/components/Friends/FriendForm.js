e1df36e996163559785f78ca6ed55d81ce678bc4
Austin637 <austinesparza@gmail.com>
2019-04-04 09:27:54 -0700

    formatting, POST for FriendForm

--- friends/src/components/Friends/FriendForm.js
+++ friends/src/components/Friends/FriendForm.js
@@ -1,16 +1,69 @@
 import React from "react";
 
-const FriendForm = () => {
-  return (
-    <div>
-      <form>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="number" name="age" placeholder="Age" />
-        <input type="email" name="email" placeholder="Email" />
-        <button>Add friend</button>
-      </form>
-    </div>
-  );
-};
+class FriendForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friend: {
+        name: "",
+        age: 0, //state currently updates this as a string
+        email: ""
+      }
+    };
+  }
+
+  handleChange = e => {
+    if (e.target.name === "age") {
+      console.log("got it");
+      let numberValue = Number(e.target.value);
+
+      this.setState({
+        friend: {
+          ...this.state.friend,
+          [e.target.name]: numberValue
+        }
+      });
+    } else {
+      this.setState({
+        friend: {
+          ...this.state.friend,
+          [e.target.name]: e.target.value
+        }
+      });
+    }
+  };
+
+  addFriendToList = e => {
+    e.preventDefault();
+    this.props.addFriendToList(this.state.friend);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.addFriendToList}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            name="age"
+            placeholder="Age"
+            onChange={this.handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={this.handleChange}
+          />
+          <button>Add friend</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default FriendForm;
